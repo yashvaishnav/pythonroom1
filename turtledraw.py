48ae038a9f7e565fe64369c03c6583db6232a1d8
yashvaishnav <yashvaishnav@gmail.com>
2014-04-23 16:16:54 -0700

    learned how to use a for loop

--- turtledraw.py
+++ turtledraw.py
@@ -2,14 +2,14 @@
 
 import turtle
 
-length = 100
-angle = 90
-count = 0
+n = 8
+length = 70
+angle = 180 - (180 * (n - 2) / n)
+numbers = [ 1, 2, 3, 4, 5, 6, 7, 8 ]
 
 t = turtle.Turtle()
 
-
-while count < 4:	
+for number in numbers:
 	t.forward(length)
 	t.left(angle)
-	count = count + 1
+	
