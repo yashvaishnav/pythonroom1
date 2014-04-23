8718ab9556bef957fe3f6d702108a22d25a291ff
yashvaishnav <yashvaishnav@gmail.com>
2014-04-23 16:51:02 -0700

    pythonroom update

--- turtledraw.py
+++ turtledraw.py
@@ -2,14 +2,14 @@
 
 import turtle
 
-n = 8
-length = 70
-angle = 180 - (180 * (n - 2) / n)
-numbers = [ 1, 2, 3, 4, 5, 6, 7, 8 ]
+n = 100
+length = 112
+#angle = 180 - (180 * (n - 2) / n)
+colors = ["red", "blue", "green", "yellow"]
+numbers = range(0, 100)
 
 t = turtle.Turtle()
 
 for number in numbers:
-	t.forward(length)
-	t.left(angle)
-	
+	t.forward(number)
+	t.left(25)
